a500ae89d01e128d1907af1e63fc80e6b4fbabe0
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2019-12-06 21:19:23 -0600

    worked on navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { CollapseModule } from 'ngx-bootstrap'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NavbarComponent } from './shared/navbar/navbar.component'
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CollapseModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core'
 export class NavbarComponent implements OnInit {
   @Input('brand-title') brandTitle: string
 
+  isCollapsed = true
+
   constructor() {}
 
   ngOnInit() {}
