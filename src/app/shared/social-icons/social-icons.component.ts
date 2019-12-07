30cd6e49fa16ae602a3e8a31e3b177a0c70dfa8f
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2019-12-07 13:55:57 -0600

    added twitch icon

--- src/app/shared/social-icons/social-icons.component.ts
+++ src/app/shared/social-icons/social-icons.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { faGithub, faCodepen, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faCodepen, faInstagram, faTwitch } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-social-icons',
@@ -10,6 +10,7 @@ export class SocialIconsComponent implements OnInit {
   github = faGithub
   codepen = faCodepen
   instagram = faInstagram
+  twitch = faTwitch
 
   constructor() {}
 
