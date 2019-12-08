b18b47bd6a34e6b4009f25b380e588335933d03a
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2019-12-08 10:49:42 -0600

    Started work on blog page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NavbarComponent } from './shared/components/navbar/navbar.component'
 import { HomeComponent } from './home/home.component';
-import { SocialIconsComponent } from './shared/components/social-icons/social-icons.component'
+import { SocialIconsComponent } from './shared/components/social-icons/social-icons.component';
+import { BlogComponent } from './blog/blog.component'
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, SocialIconsComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, SocialIconsComponent, BlogComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CollapseModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
