63719d2c7ea229b02e288fddb6a18c75d980e0ae
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2019-12-08 09:49:34 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { NavbarComponent } from './shared/navbar/navbar.component'
+import { NavbarComponent } from './shared/components/navbar/navbar.component'
 import { HomeComponent } from './home/home.component';
-import { SocialIconsComponent } from './shared/social-icons/social-icons.component'
+import { SocialIconsComponent } from './shared/components/social-icons/social-icons.component'
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomeComponent, SocialIconsComponent],
