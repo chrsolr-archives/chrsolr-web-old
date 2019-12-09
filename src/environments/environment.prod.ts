fb550840c7d7178874d1c6d6b5be8ac82fcb5adb
Christian Soler <chr.solr@gmail.com>
2019-12-09 13:37:33 -0600

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCR6mN5BM9TcukTXFX2GwzTgdAMixOdFwc',
+    authDomain: 'chrsolr-2dde5.firebaseapp.com',
+    databaseURL: 'https://chrsolr-2dde5.firebaseio.com',
+    projectId: 'chrsolr-2dde5',
+    storageBucket: 'chrsolr-2dde5.appspot.com',
+    messagingSenderId: '737724921881',
+    appId: '1:737724921881:web:64c4dffcb17a00e16dd288',
+    measurementId: 'G-CFMG4N5KLL',
+  },
 };
